e6af3f922804fde3840d703884125cc66a930c42
diegoPaladino <diegopaladinofoto@gmail.com>
2021-06-28 20:43:32 -0300

    202106282043

---
+++ Main_Board_2.py
@@ -0,0 +1,28 @@
+# Main_Board_2
+
+# importgin libraries
+import os
+import tkinter
+from tkinter import *
+import tkinter.messagebox as tkmsg
+from PIL import Image, ImageTk
+
+# declarations
+def ao_clicar_login():
+    os.startfile("")
+    
+def ao_clicar_pesquisa_aluno():
+    os.startfile("")
+
+def ao_clicar_reset_mozila():
+    os.startfile("")
+    
+def ao_clicar_cadastring_student():
+    os.startfile("")
+    
+def ao_clicar_vinculando_aluno():
+    os.startfile("")
+    
+
+# executing
+
