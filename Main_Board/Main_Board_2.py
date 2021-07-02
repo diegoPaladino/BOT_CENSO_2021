9b34404196e1ee7e2143d557afb90d97984b368f
diegoPaladino <diegopaladinofoto@gmail.com>
2021-07-02 07:36:30 -0300

    202107020735

--- Main_Board/Main_Board_2.py
+++ Main_Board/Main_Board_2.py
@@ -20,7 +20,7 @@ def ao_clicar_pesquisa_aluno():
     os.startfile("002-pesquisa_aluno_atalho")
     
 def ao_clicar_b_pesquisa_aluno():
-    os.startfile("002-b-pesquisa_aluno_atalho")
+    os.startfile("002-b-pesquisa_aluno_gemul_atalho")
 
 def ao_clicar_reset_mozila():
     os.startfile("003-reset_mozila_censo_atalho")
@@ -45,7 +45,7 @@ window.geometry("%dx%d+0+0" % (w, h))
 # window.geometry("1600x900+200+100")
 window.title("CENSO - 2021 - diegoPaladino")
 
-img = PhotoImage(file="main_background.png")
+img = PhotoImage(file="main_background_4.png")
 label = Label(
     window,
     image=img
@@ -57,27 +57,28 @@ label.place(x=0, y=0)
 
 
 # Bottons
+cor_botao='#01ae0d'
 
-botao_1 = Button(window, text="Login", command=ao_clicar_login, bg='SeaGreen2')
+botao_1 = Button(window, text="Login", command=ao_clicar_login, bg=cor_botao)
 botao_1.place(x=20,y=20)
 
-botao_2 = Button(window, text="Pesquisa", command=ao_clicar_pesquisa_aluno, bg='SeaGreen2')
-botao_2.place(x=100,y=50)
+botao_2 = Button(window, text="Pesquisa A", command=ao_clicar_pesquisa_aluno, bg=cor_botao)
+botao_2.place(x=20,y=80)
 
-botao_2_b = Button(window, text="Pesquisa", command=ao_clicar_pesquisa_aluno, bg='SeaGreen2')
-botao_2_b.place(x=100,y=50)
+botao_2_b = Button(window, text="Pesquisa B", command=ao_clicar_pesquisa_aluno, bg=cor_botao)
+botao_2_b.place(x=110,y=80)
 
-botao_3 = Button(window, text="Reset", command=ao_clicar_reset_mozila, bg='SeaGreen2')
-botao_3.place(x=80,y=20)
+botao_3 = Button(window, text="Reset", command=ao_clicar_reset_mozila, bg=cor_botao)
+botao_3.place(x=410,y=20)
 
-botao_4 = Button(window, text="Cadastrar", command=ao_clicar_cadastring_student, bg='SeaGreen2')
-botao_4.place(x=240,y=50)
+botao_4 = Button(window, text="Cadastrar A", command=ao_clicar_cadastring_student, bg=cor_botao)
+botao_4.place(x=200,y=80)
 
-botao_4_b = Button(window, text="Cadastrar", command=ao_clicar_cadastring_student, bg='SeaGreen2')
-botao_4_b.place(x=240,y=50)
+botao_4_b = Button(window, text="Cadastrar B", command=ao_clicar_cadastring_student, bg=cor_botao)
+botao_4_b.place(x=300,y=80)
 
-botao_5 = Button(window, text="Vincular", command=ao_clicar_vinculando_aluno, bg='SeaGreen2')
-botao_5.place(x=320,y=50)
+botao_5 = Button(window, text="Vincular", command=ao_clicar_vinculando_aluno, bg=cor_botao)
+botao_5.place(x=395,y=80)
 
 
 # executing
