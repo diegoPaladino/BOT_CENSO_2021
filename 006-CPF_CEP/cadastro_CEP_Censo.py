77e553fa0329f2c081f612b57ab911c656613bf3
diegoPaladino <diegopaladinofoto@gmail.com>
2021-08-19 06:30:54 -0300

    202108190630

--- 006-CPF_CEP/cadastro_CEP_Censo.py
+++ 006-CPF_CEP/cadastro_CEP_Censo.py
@@ -42,6 +42,38 @@ def enviar_CEP():
     p.moveTo(x=1295, y=-170,duration=0.2)
     p.click()
     
+def select_visual_studio():
+    p.moveTo(x=366, y=-23,duration=0.2)
+    p.click()
+    
+    
+################################
+#Selecting Opera
+################################
+def select_opera():
+    p.moveTo(x=225, y=-22,duration=0.2)
+    p.click()
+    t.sleep(0.3)
+    
+def select_first_opera_window():
+    p.moveTo(x=125, y=-885,duration=0.2)
+    p.click()
+    t.sleep(0.3)
+    
+def select_first_window():
+    p.moveTo(x=108, y=-888,duration=0.2)
+    p.click()
+    t.sleep(0.3)
+    
+def refresh_enrollment():
+    p.moveTo(x=254, y=-709,duration=0.2)
+    p.click()
+    t.sleep(0.3)
+    p.moveTo(x=237, y=-603,duration=0.2)
+    p.click()
+    t.sleep(0.3)
+    
+
     
 ################################
 
@@ -57,8 +89,15 @@ def clicking_white_area():
     p.moveTo(x=230, y=-281,duration=0.2)
     p.click()
     t.sleep(0.2)
-
     
+def home_home():
+    p.hotkey('home')
+    p.sleep(0.2)
+    
+def esc_esc():
+    p.hotkey('esc')
+    t.sleep(0.3)
+
 #################################
 alert()
 select_opera()
@@ -70,6 +109,13 @@ write_CEP()
 clicking_white_area()
 down_down()
 enviar_CEP()
+home_home()
+#refreshing opera:
+select_opera()
+select_first_opera_window()
+select_first_window()
+refresh_enrollment()
+select_visual_studio()
 
 
 
